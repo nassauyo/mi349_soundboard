e8a9b8f047d73d3589080845fc44042d1d0edb7c
nassauyo <nassauyo@msu.edu>
2020-11-15 22:25:28 -0500

    soundboard project posting

---
+++ soundboard.js
@@ -0,0 +1,14 @@
+function playSound1() {
+    var buttonElementChime = document.getElementById("chimefx");
+    buttonElementChime.play();
+}
+
+function playSound2() {
+    var buttonElementBell = document.getElementById("bellfx");
+    buttonElementBell.play();
+}
+
+function playSound3() {
+    var buttonElementBoing = document.getElementById("boingfx");
+    buttonElementBoing.play();
+}
